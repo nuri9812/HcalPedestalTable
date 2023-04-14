f135431f9a2b5a9f13894307d6d51a37bbcc9ed5
해연 황 <nuri9812@soongsil.ac.kr>
2023-04-14 15:30:38 +0900

    add lines to print status

--- Pedestal/run.sh
+++ Pedestal/run.sh
@@ -2,6 +2,7 @@
 
 #./run.sh <runNumber>
 
+echo 'It takes about 5~10 minuet...'
 start=`date +%s.%N`
 
 
@@ -13,9 +14,9 @@ start=`date +%s.%N`
 #root -b -q $CMSSW_BASE'/src/Pedestal/HCALPFG/HcalPedestalTuning/HcalPedestalTable/HCALPedestalTableMaker.C++("'$CMSSW_BASE'/src/Pedestal/nTuple.root")' #update your path
 
 python PedestalTableMakerHcalNano-pyRoot.py $1
-echo "initial done"
+echo "initial table is produced"
 python3 interpolation.py nTuple_PedestalTable.txt
-
+echo "missing channel interpolation is done"
 cat nTuple_PedestalTable_interploated.txt | grep -e HB -e HE >> nTuple_PedestalTable_interploated_HBHE.txt
 cat nTuple_PedestalTable_interploated.txt | grep -e HF -e HO >> nTuple_PedestalTable_interploated_HFHO.txt
 
@@ -29,9 +30,10 @@ mkdir Fig
 root -b -q $CMSSW_BASE'/src/Pedestal/HCALPFG/HcalPedestalTuning/HcalPedestalTable/HCALPedestalAnalysis.C++("'$CMSSW_BASE'/src/Pedestal/nTuple_PedestalTable_interploated_HFHO.txt",30,0,70,15,0,50)' #update your path
 mv Fig Fig_HFHO
 
+echo 'plots for saniti check is produced'
 root -b -q ProduceWidths.cc
 
-
+echo 'pedestal width is extracted'
 finish=`date +%s.%N`
 diff=$( echo "$finish - $start" | bc -l )
 
