85356d77a7744d87220609626eef525c44821eb9
해연 황 <nuri9812@soongsil.ac.kr>
2023-04-14 15:37:48 +0900

    add line to print out status

--- Pedestal/run.sh
+++ Pedestal/run.sh
@@ -2,6 +2,9 @@
 
 #./run.sh <runNumber>
 
+echo "start"
+echo "it takes 5~10 minuit"
+
 start=`date +%s.%N`
 
 
@@ -13,8 +16,10 @@ start=`date +%s.%N`
 #root -b -q $CMSSW_BASE'/src/Pedestal/HCALPFG/HcalPedestalTuning/HcalPedestalTable/HCALPedestalTableMaker.C++("'$CMSSW_BASE'/src/Pedestal/nTuple.root")' #update your path
 
 python PedestalTableMakerHcalNano-pyRoot.py $1
-echo "initial done"
+echo "initial table is produced"
+
 python3 interpolation.py PedestalTable.txt
+echo "interploation is done"
 
 cat PedestalTable_interploated.txt | grep -e HB -e HE >> PedestalTable_interploated_HBHE.txt
 cat PedestalTable_interploated.txt | grep -e HF -e HO >> PedestalTable_interploated_HFHO.txt
@@ -29,12 +34,15 @@ mkdir Fig
 root -b -q $CMSSW_BASE'/src/Pedestal/HCALPFG/HcalPedestalTuning/HcalPedestalTable/HCALPedestalAnalysis.C++("'$CMSSW_BASE'/src/Pedestal/PedestalTable_interploated_HFHO.txt",30,0,70,15,0,50)' #update your path
 mv Fig Fig_HFHO
 
+echo "plots for sanity check is produced"
+
 root -b -q ProduceWidths.cc
 
+echo "Pedestal Width is extracted"
 
 finish=`date +%s.%N`
 diff=$( echo "$finish - $start" | bc -l )
 
-echo 'start:' $start
-echo 'finish:' $finish
+echo 'start at' $start
+echo 'finish at' $finish
 echo 'diff:' $diff
